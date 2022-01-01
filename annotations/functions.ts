fc132a135fd51d660d551db94f7501d5bfa6105e
Mokhmad <mokhmad.tazbiev@gmail.com>
2022-01-01 11:09:26 +0300

    FunctionsAnnotation

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,15 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const subtrack = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiple = function (a: number, b: number): number {
+  return a * b;
+};
