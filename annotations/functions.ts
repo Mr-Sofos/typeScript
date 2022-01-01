c2a051f7e600320f18dabbe69b9090ae0709e278
Mokhmad <mokhmad.tazbiev@gmail.com>
2022-01-01 11:32:01 +0300

    destructuringAnnotation

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -13,3 +13,29 @@ function divide(a: number, b: number): number {
 const multiple = function (a: number, b: number): number {
   return a * b;
 };
+
+const looger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const todayWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todayWeather);
